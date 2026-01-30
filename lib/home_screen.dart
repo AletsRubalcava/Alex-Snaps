047b16a1efadefebaed7e9d633e9bd71cc26afc6
Alejandro <alejandrorubalcavav@gmail.com>
2026-01-30 16:56:09 -0600

    Updated Home Screen for it to be stateful in order to upgrade app performance

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,182 +6,197 @@ import 'assets.dart';
 import 'strings.dart';
 import 'package:alex_snaps/home_photo_button.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomeScreen> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen>{
+
+  @override
+  void initState() {
+    super.initState();
+
     SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light),
+      const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarColor: Colors.transparent,
+      ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
 
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double mainButtonAspectRatio;
-        double secondaryButtonAspectRatio;
+    final iconSize = width * 0.12;
 
-        if (height < 700) {
-          mainButtonAspectRatio = 5 / 4;
-          secondaryButtonAspectRatio = 4 / 5;
-        } else {
-          mainButtonAspectRatio = 1;
-          secondaryButtonAspectRatio = 5 / 8;
-        }
+    double mainButtonAspectRatio;
+    double secondaryButtonAspectRatio;
 
-        return Scaffold(
-          backgroundColor: Color(0xFF2D2D2D),
-          body: Padding(
-            padding: EdgeInsets.only(
-              top: height * 0.05,
-              left: width * 0.08,
-              right: width * 0.08,
-            ),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SvgPicture.asset(
-                      Assets.icons.alexSnapsLogo,
-                      colorFilter: ColorFilter.mode(
-                        Theme.of(context).primaryColor,
-                        BlendMode.srcIn,
-                      ),
-                      width: width * 0.15,
+    if (height < 700) {
+      mainButtonAspectRatio = 5 / 4;
+      secondaryButtonAspectRatio = 4 / 5;
+    } else {
+      mainButtonAspectRatio = 1;
+      secondaryButtonAspectRatio = 5 / 8;
+    }
+
+    return Scaffold(
+      backgroundColor: Color(0xFF2D2D2D),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: height * 0.01,
+            left: width * 0.08,
+            right: width * 0.08,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset(
+                    Assets.icons.alexSnapsLogo,
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).primaryColor,
+                      BlendMode.srcIn,
                     ),
-                    Text(
-                      Strings.appName,
-                      style: GoogleFonts.leagueGothic(
-                        fontSize: 30,
-                        color: Theme.of(context).primaryColor,
-                      ),
+                    width: width * 0.15,
+                  ),
+                  Text(
+                    Strings.appName,
+                    style: GoogleFonts.leagueGothic(
+                      fontSize: width * 0.07,
+                      color: Theme.of(context).primaryColor,
                     ),
-                  ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.only(top: height * 0.005),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'HOME',
+                    style: GoogleFonts.leagueGothic(
+                      color: Colors.white,
+                      fontSize: width * 0.10,
+                    ),
+                  ),
                 ),
-                Padding(
-                  padding: EdgeInsetsGeometry.only(top: height*0.005),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
+              ),
+              AspectRatio(
+                aspectRatio: mainButtonAspectRatio,
+                child: HomePhotoButton(
+                  photo: 'assets/images/IMG_0701.jpg',
+                  text: 'VIEW ALL',
+                ),
+              ),
+              Spacer(),
+              Row(
+                spacing: 10,
+                children: [
+                  Flexible(
+                    child: AspectRatio(
+                      aspectRatio: secondaryButtonAspectRatio,
+                      child: HomePhotoButton(
+                        photo: 'assets/images/IMG_1661.jpg',
+                        text: 'VASCONCELOS',
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: AspectRatio(
+                      aspectRatio: secondaryButtonAspectRatio,
+                      child: HomePhotoButton(
+                        photo: 'assets/images/IMG_1781.jpg',
+                        text: 'WILDLIFE',
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: AspectRatio(
+                      aspectRatio: secondaryButtonAspectRatio,
+                      child: HomePhotoButton(
+                        photo: 'assets/images/IMG_0775.jpg',
+                        text: 'VERACRUZ',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(20),
+                child: Container(
+                  height: height * 0.07,
+                  width: double.infinity,
+                  color: Color(0xFF5D5D5D),
+                  child: Center(
                     child: Text(
-                      'HOME',
+                      'LEARN ABOUT ME',
                       style: GoogleFonts.leagueGothic(
                         color: Colors.white,
-                        fontSize: 40,
+                        fontSize: width * 0.08,
                       ),
                     ),
-                  ),
-                ),
-                AspectRatio(
-                  aspectRatio: mainButtonAspectRatio,
-                  child: HomePhotoButton(
-                    photo: 'assets/images/IMG_0701.jpg',
-                    text: 'VIEW ALL',
-                  ),
-                ),
-                Spacer(),
-                Row(
-                  spacing: 10,
-                  children: [
-                    Flexible(
-                      child: AspectRatio(
-                        aspectRatio: secondaryButtonAspectRatio,
-                        child: HomePhotoButton(
-                          photo: 'assets/images/IMG_1661.jpg',
-                          text: 'VASCONCELOS',
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: AspectRatio(
-                        aspectRatio: secondaryButtonAspectRatio,
-                        child: HomePhotoButton(
-                          photo: 'assets/images/IMG_1781.jpg',
-                          text: 'WILDLIFE',
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: AspectRatio(
-                        aspectRatio: secondaryButtonAspectRatio,
-                        child: HomePhotoButton(
-                          photo: 'assets/images/IMG_0775.jpg',
-                          text: 'VERACRUZ',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                ClipRRect(
-                  borderRadius: BorderRadiusGeometry.circular(20),
-                  child: Container(
-                    height: height * 0.07,
-                    width: double.infinity,
-                    color: Color(0xFF5D5D5D),
-                    child: Center(
-                      child: Text(
-                        'LEARN ABOUT ME',
-                        style: GoogleFonts.leagueGothic(
-                          color: Colors.white,
-                          fontSize: 30,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Spacer(),
-              ],
-            ),
-          ),
-          bottomNavigationBar: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.10,
-            child: ClipRRect(
-              borderRadius: BorderRadiusGeometry.only(
-                topLeft: Radius.circular(25),
-                topRight: Radius.circular(25),
-              ),
-              child: Container(
-                color: Theme.of(context).primaryColor,
-                child: Padding(
-                  padding: EdgeInsetsGeometry.symmetric(horizontal: 40),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icons/layout-grid.svg',
-                        width: MediaQuery.of(context).size.width * 0.12,
-                        colorFilter: ColorFilter.mode(
-                          Color(0xFF316163),
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/icons/home.svg',
-                        width: MediaQuery.of(context).size.width * 0.12,
-                        colorFilter: ColorFilter.mode(
-                          Color(0xFF316163),
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/icons/user.svg',
-                        width: MediaQuery.of(context).size.width * 0.12,
-                        colorFilter: ColorFilter.mode(
-                          Color(0xFF316163),
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ],
                   ),
                 ),
               ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: height * 0.10,
+        child: ClipRRect(
+          borderRadius: BorderRadiusGeometry.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          ),
+          child: Container(
+            color: Theme.of(context).primaryColor,
+            child: Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/layout-grid.svg',
+                    width: iconSize,
+                    colorFilter: ColorFilter.mode(
+                      Color(0xFF316163),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  SvgPicture.asset(
+                    'assets/icons/home.svg',
+                    width: iconSize,
+                    colorFilter: ColorFilter.mode(
+                      Color(0xFF316163),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  SvgPicture.asset(
+                    'assets/icons/user.svg',
+                    width: iconSize,
+                    colorFilter: ColorFilter.mode(
+                      Color(0xFF316163),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
