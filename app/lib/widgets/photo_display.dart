32ce769b3d6808a4a68d36e76037afb594946f82
Alejandro <alejandrorubalcavav@gmail.com>
2026-03-14 16:52:07 -0600

    Added cache width to photo display for better asset performance

--- app/lib/widgets/photo_display.dart
+++ app/lib/widgets/photo_display.dart
@@ -1,5 +1,5 @@
 import 'package:alex_snaps/widgets/show_image_dialog.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PhotoDisplay extends StatelessWidget{
   const PhotoDisplay({required this.photo,super.key});
@@ -8,16 +8,29 @@ class PhotoDisplay extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+
+    // Calculamos el tamaño aproximado de la celda en la grid (3 columnas)
+    // Pantalla - (padding 8% * 2) / 3 columnas
+    final cacheWidth = ((width * 0.84) / 3 * devicePixelRatio * 0.5).round();
+
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         showImageDialog(context, photo);
       },
       child: Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadiusGeometry.circular(5),
-        image: DecorationImage(image: AssetImage(photo), fit: BoxFit.cover),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          image: DecorationImage(
+            image: ResizeImage(
+              AssetImage(photo),
+              width: cacheWidth,
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
-      )
     );
   }
 }
